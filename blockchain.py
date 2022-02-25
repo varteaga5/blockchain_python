34f0c7e71a06614abb649d42f2a6f0291ef5a6bf
vincent <vincent.arteaga@gmail.com>
2022-02-24 21:03:54 -0800

    working with keyword args

--- blockchain.py
+++ blockchain.py
@@ -8,7 +8,7 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 add_value(2)
-add_value(0.9, get_last_blockchain_value())
+add_value(transaction_amount=0.9, last_transaction=get_last_blockchain_value())
 add_value(10.89, get_last_blockchain_value())
 
 print(blockchain)

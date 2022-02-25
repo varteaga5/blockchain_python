21c77df0b049ec261ed550be101e2e9ef7a18e79
vincent <vincent.arteaga@gmail.com>
2022-02-24 20:39:29 -0800

    updated add_value

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+blockchain = [[1]]
+
+def add_value(transaction_amount):
+    blockchain.append([blockchain[-1], transaction_amount])
+    print(blockchain)
+
+add_value(2)
+add_value(0.9)
+add_value(10.89)
+
+[[1], [[1], 2]]
+[[1], [[1], 2], [[[1], 2], 0.9]]
+[[1], [[1], 2], [[[1], 2], 0.9], [[[[1], 2], 0.9], 10.89]] 
--- bockchain.py
+++
@@ -1,5 +0,0 @@
-blockchain = [1]
-
-def add_value():
-    blockchain.append([blockchain[0], 5.3])
-    print(blockchain)
